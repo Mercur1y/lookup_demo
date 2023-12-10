bea6d015f14ce94a7fbdc596d610a231be2d894a
Глеб Приставка <g.pristavka@bftcom.com>
2023-12-11 01:55:08 +0400

    Initial commit

---
+++ src/main/java/com/pet_projects/lookup_demo/LookupDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pet_projects.lookup_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookupDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LookupDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pet_projects/lookup_demo/LookupDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pet_projects.lookup_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookupDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
